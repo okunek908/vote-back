399ac52193e3ae905e68e5418eb1bff15e994c10
okunek <okunek908@gmail.com>
2021-01-28 12:40:54 +0300

    jdk 11->15

--- src/main/java/digital/future/vote/backend/domain/Participant.java
+++ src/main/java/digital/future/vote/backend/domain/Participant.java
@@ -1,10 +1,9 @@
 package digital.future.vote.backend.domain;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.annotation.JsonValue;
+import io.micronaut.core.annotation.Creator;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.data.annotation.TypeDef;
-import io.micronaut.data.model.DataType;
 import lombok.*;
 
 import javax.annotation.concurrent.Immutable;
@@ -19,4 +18,9 @@ public class Participant {
     // Opaque random-like user id
     @JsonValue
     @NonNull String voterId;
+
+    @Creator
+    public static Participant fromString(String voterId) {
+        return new Participant(voterId);
+    }
 }
