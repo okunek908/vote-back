ebf6426462b396e4518ad7ad0d375d23428d7594
zorka <zorka1610@prootonmail.com>
2021-02-06 01:23:32 +0300

    Status actions controller

--- src/main/java/digital/future/vote/backend/domain/StatusActions.java
+++ src/main/java/digital/future/vote/backend/domain/StatusActions.java
@@ -2,16 +2,16 @@ package digital.future.vote.backend.domain;
 
 import lombok.Data;
 
-import java.util.Set;
+import java.util.List;
 
 @Data
 public class StatusActions {
 
     private Poll.Status status;
 
-    private Set<Action> actions;
+    private List<Action> actions;
 
-    public StatusActions(Poll.Status status, Set<Action> actions) {
+    public StatusActions(Poll.Status status, List<Action> actions) {
         this.status = status;
         this.actions = actions;
     }
---
+++ src/main/java/digital/future/vote/backend/domain/StatusActionsRepo.java
@@ -0,0 +1,27 @@
+package digital.future.vote.backend.domain;
+
+import com.google.common.collect.Lists;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class StatusActionsRepo {
+
+    private List<StatusActions> statusActionsSet;
+
+    {
+        statusActionsSet = new ArrayList<>();
+        statusActionsSet.add(new StatusActions(Poll.Status.DRAFT, Lists.newArrayList(StatusActions.Action.DELETE, StatusActions.Action.EDIT)));
+        statusActionsSet.add(new StatusActions(Poll.Status.PUBLISHED, Lists.newArrayList(StatusActions.Action.DELETE, StatusActions.Action.EDIT)));
+        statusActionsSet.add(new StatusActions(Poll.Status.ACTIVE, Lists.newArrayList(StatusActions.Action.VIEW)));
+        statusActionsSet.add(new StatusActions(Poll.Status.ENDED, Lists.newArrayList(StatusActions.Action.VIEW)));
+    }
+
+    public List<StatusActions.Action> getActionsByStatus(String status) {
+        return statusActionsSet.stream().findFirst()
+                .filter(statusActions -> status.equals(statusActions.getStatus().name())).get().getActions();
+    }
+}
