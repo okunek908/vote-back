2ee3d361ef662fb0409020b3d868ed140f86922c
zorka <zorka1610@prootonmail.com>
2021-02-11 18:13:52 +0300

    StatusActionsRepo deleted

--- src/main/java/digital/future/vote/backend/controller/ActionsController.java
+++ src/main/java/digital/future/vote/backend/controller/ActionsController.java
@@ -1,22 +1,31 @@
 package digital.future.vote.backend.controller;
 
+import com.google.common.collect.Lists;
+import digital.future.vote.backend.domain.Poll;
 import digital.future.vote.backend.domain.StatusActions;
-import digital.future.vote.backend.domain.StatusActionsRepo;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 
-import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller("actions")
 public class ActionsController {
 
-    @Inject
-    StatusActionsRepo statusActionsRepo;
+    private List<StatusActions> statusActionsSet;
+
+    {
+        statusActionsSet = new ArrayList<>();
+        statusActionsSet.add(new StatusActions(Poll.Status.DRAFT, Lists.newArrayList(StatusActions.Action.DELETE, StatusActions.Action.EDIT)));
+        statusActionsSet.add(new StatusActions(Poll.Status.PUBLISHED, Lists.newArrayList(StatusActions.Action.DELETE, StatusActions.Action.EDIT)));
+        statusActionsSet.add(new StatusActions(Poll.Status.ACTIVE, Lists.newArrayList(StatusActions.Action.VIEW)));
+        statusActionsSet.add(new StatusActions(Poll.Status.ENDED, Lists.newArrayList(StatusActions.Action.VIEW)));
+    }
 
     @Get
-    public List<StatusActions.Action> getActions(@QueryValue String status) {
-        return statusActionsRepo.getActionsByStatus(status);
+    public List<StatusActions.Action> getActionsByStatus(@QueryValue String status) {
+        return statusActionsSet.stream().findFirst()
+                .filter(statusActions -> status.equals(statusActions.getStatus().name())).get().getActions();
     }
 }
